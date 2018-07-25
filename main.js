7ad708e4801077a80cfe7abdb9607a294c20b7fa
chauncey <chchuang@foxmail.com>
2018-07-25 09:19:52 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+window.jQuery = function(nodeOrSelector){
+    let nodes = {}
+    nodes.addClass = function(){}
+    nodes.html = function(){}
+    return nodes
+  }
+  window.$ = window.jQuery
+
+  window.jQuery.ajax = function({url, method, body, headers,success, fail}){
+      let request = new XMLHttpRequest()
+      request.open(method, url) // 配置request
+      for(let key in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+      }
+      request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+          if(request.status >= 200 && request.status < 300){
+            success.call(undefined, request.responseText)
+          }else if(request.status >= 400){
+            fail.call(undefined, request)
+          }
+        }
+      }
+      request.send(body)
+  }
+  
+function f1(responseText){}
+
+myButton.addEventListener('click', (e)=>{
+    window.jQuery.ajax({
+      url: '/xxx',
+      method: 'get',
+      headers: {
+        'content-type':'application/x-www-form-urlencoded',
+        'xxx': '18'
+      },
+      success: (x)=>{
+        f1.call(undefined,x)
+      },
+      fail: (x)=>{
+        console.log(x)
+        console.log(x.status)
+        console.log(x.responseText)
+      }
+    })
+  })
